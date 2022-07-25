c5cc2147c181fb720439817c3a9a8d7c61e32728
ashwinin2 <106053334+ashwinin2@users.noreply.github.com>
2022-07-25 13:49:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,22 +17,22 @@ provider "azurerm" {
 }
 
 # Later we need the Vnet and subnet for our infrastructure
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "Ashwini" {
+  name     = "Ash-Resources"
   location = "we can give the location here for example: East US"
 }
 # Network Security group is also very important wen we create the Virtual network  
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location # here it is going to reference the location of our resource group created.
-  resource_group_name = azurerm_resource_group.example.name # here it is going to reference the name  of our resource group created.
+resource "azurerm_network_security_group" "Ashwini" {
+  name                = "Ash-security-group"
+  location            = azurerm_resource_group.Ashwini.location # here it is going to reference the location of our resource group created.
+  resource_group_name = azurerm_resource_group.Ashwini.name # here it is going to reference the name  of our resource group created.
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "Ashwini" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.Ashwini.location
+  resource_group_name = azurerm_resource_group.Ashwini.name
   address_space       = ["10.0.0.0/16"] # Here address space is nothing but the Range of Ip Address ryt , so azure will assign the next availble Ip addr from this Addrs space to a resource in our Virtual network.
   The above ["10.0.0.0/16"] is just an example and it falls under Class A Networks.
   
@@ -46,7 +46,7 @@ resource "azurerm_virtual_network" "example" {
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
+    security_group = azurerm_network_security_group.Ashwini.id
  
   }
 }
@@ -54,10 +54,10 @@ resource "azurerm_virtual_network" "example" {
 # Once like all this Resource group, Vnet, subnets etc is created we can now create our Virtual machines for our web servers .
 
 
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "Ashwini" {
   name                = "Vm1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.Ashwini.name
+  location            = azurerm_resource_group.Ashwini.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
 
@@ -81,13 +81,13 @@ resource "azurerm_linux_virtual_machine" "example" {
 }
 # we are now going to create an Azure load balancer if there is any traffic to this web server so that this can balance the traffic in this.
 
-resource "azurerm_lb" "example" {
+resource "azurerm_lb" "Ashwini" {
   name                = "TestLoadBalancer"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.Ashwini.location
+  resource_group_name = azurerm_resource_group.Ashwini.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    public_ip_address_id = azurerm_public_ip.Ashwini.id
   }
 }
